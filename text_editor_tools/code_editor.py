15a7d252c71654f2c024ea3a6e378dd47e2600f4
tin2tin <tintwotin@gmail.com>
2020-02-02 17:20:10 +0100

    Updated to 2.8+ by kaio
    https://github.com/K-410/blender-scripts/blob/master/2.8/code_editor.py

--- text_editor_tools/code_editor.py
+++ text_editor_tools/code_editor.py
@@ -1,4 +1,4 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+# ###### BEGIN GPL LICENSE BLOCK ######
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -14,7 +14,7 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-# ##### END GPL LICENSE BLOCK #####
+# ###### END GPL LICENSE BLOCK ######
 
 # bl_info = {
 #     "name": "Code Editor",
